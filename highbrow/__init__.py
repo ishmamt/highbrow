3311b69c56a83b92272432c74409c4bee84c01ba
ishmamt <55696609+ishmamt@users.noreply.github.com>
2021-05-11 12:46:46 +0600

    UI user profile page

--- highbrow/__init__.py
+++ highbrow/__init__.py
@@ -4,11 +4,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # from highbrow.users.routes import users
+    from highbrow.users.routes import users
     # from highbrow.posts.routes import posts
     from highbrow.main.routes import main
 
-    # app.register_blueprint(users)
+    app.register_blueprint(users)
     # app.register_blueprint(posts)
     app.register_blueprint(main)
 
--- highbrow/users/routes.py
+++ highbrow/users/routes.py
@@ -1,3 +1,8 @@
 from flask import render_template, url_for, redirect, request, Blueprint
 
 users = Blueprint('users', __name__)  # similar to app = Flask(__name__)
+
+
+@users.route("/user")
+def user():
+    return render_template("user.html")
