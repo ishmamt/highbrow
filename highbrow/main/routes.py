8952c3f3383fcb25196a211d212391835fdf8f18
ishmamt <55696609+ishmamt@users.noreply.github.com>
2021-07-03 04:58:21 +0600

    Backend Skeleton for creating posts

--- highbrow/main/routes.py
+++ highbrow/main/routes.py
@@ -1,10 +1,54 @@
 from flask import render_template, request, Blueprint
+from highbrow.main.forms import PostForm
 
 main = Blueprint('main', __name__)  # similar to app = Flask(__name__)
 
 
-@main.route("/")
-@main.route("/home")
-@main.route("/index")
+posts = [
+    {
+        "username": "Tawseef Tajwar",
+        "time": 3,
+        "title": "Hello World",
+        "content": "Testing 123",
+        "tags": ["HTML", "CSS", "Flask"],
+        "likes": 25,
+        "comments": 4
+    },
+    {
+        "username": "Ishmam",
+        "time": 3,
+        "title": "First Post",
+        "content": "The website is live and this is my very first post",
+        "tags": ["HTML", "CSS", "Flask", "Python", "Webdev"],
+        "likes": 125,
+        "comments": 533
+    },
+    {
+        "username": "Nafis",
+        "time": 3,
+        "title": "Eta ki free?",
+        "content": "Etae taka deya lagbe ki?",
+        "tags": ["Question", "Confused", "Help"],
+        "likes": 525,
+        "comments": 14
+    }
+]
+
+
+@main.route("/", methods=["GET", "POST"])
+@main.route("/home", methods=["GET", "POST"])
+@main.route("/index", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    form = PostForm()
+    if form.validate_on_submit() and request.method == "POST":
+        post_entry = {
+            "username": "Tauseef Tajwar",
+            "time": 0,
+            "title": form.title.data,
+            "content": form.content.data,
+            "tags": form.topic.data.split(),
+            "likes": 0,
+            "comments": 0
+        }
+        posts.append(post_entry)
+    return render_template("home.html", posts=posts, form=form)
